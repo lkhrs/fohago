79b900c99abe9fbe6a6de33e9567985f7b7b3826
Luke Harris <luke+git@lkhrs.com>
2024-05-19 12:00:49 -0500

    Add config

---
+++ config.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/BurntSushi/toml"
+	"github.com/caarlos0/env"
+)
+
+type Config struct {
+	Global struct {
+		Port      int      `env:"PORT" envDefault:"8080"`
+		Blocklist []string `env:"BLOCKLIST" envSeparator:","`
+	}
+	Smtp struct {
+		User     string `env:"SMTP_USER"`
+		Password string `env:"SMTP_PASS"`
+		Host     string `env:"SMTP_HOST" envDefault:"localhost"`
+		Port     int    `env:"SMTP_PORT" envDefault:"1025"`
+	}
+	Api struct {
+		Akismet            string `env:"AKISMET_KEY"`
+		CloudFlare         string `env:"CLOUDFLARE_KEY"`
+		GoogleSafeBrowsing string `env:"GOOGLE_KEY"`
+	}
+	Form struct {
+		Fields   FormFields
+		Template string
+		Id       string
+	}
+}
+
+// check the config for required fields
+func (c *Config) check() error {
+	if c.Global.Port == 0 {
+		return fmt.Errorf("PORT is required")
+	}
+	if c.Smtp.Host == "" {
+		return fmt.Errorf("SMTP_HOST is required")
+	}
+	if c.Smtp.Port == 0 {
+		return fmt.Errorf("SMTP_PORT is required")
+	}
+	return nil
+}
+
+// loads the configuration from the environment
+func loadFromEnv(cfg *Config) error {
+	fields := []interface{}{
+		&cfg.Global,
+		&cfg.Smtp,
+		&cfg.Api,
+	}
+
+	for _, field := range fields {
+		if err := env.Parse(field); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// loads the configuration from a TOML file
+func loadFromToml(cfg *Config, path string) error {
+	_, err := toml.DecodeFile(path, cfg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
