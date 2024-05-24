b2fc1bbd4433a28b7036e14c5c000e8c7f648413
Luke Harris <luke+git@lkhrs.com>
2024-05-23 23:24:25 -0500

    Add utils

---
+++ utils.go
@@ -0,0 +1,11 @@
+package fohago
+
+import "net/http"
+
+// GetField returns the value of a field from a form submission
+func GetField(r *http.Request, field string) (string, error) {
+	if err := r.ParseForm(); err != nil {
+		return "", err
+	}
+	return r.Form.Get(field), nil
+}
